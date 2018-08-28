74ef40253fc5d309239ecd3c4cc16bede897da69
Miss Araquel <missaraquel@gmail.com>
2018-08-27 21:09:16 -0700

    adding friend array

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+// DATA
+// Below data will hold all of the possible friend matches.
+
+var friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+      
+  ];
+  //Export array so it's accessible to other files using require.
+  module.exports = friends;
